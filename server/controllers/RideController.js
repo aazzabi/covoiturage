9b9aa305c427e4b832a16a29af1164264fa143ff
Ala Eddin Tabai <alaeddin.tabai@esprit.tn>
2020-03-25 02:06:53 +0100

    Ride Full Back (total calculate , add,remove traveler to ride , Ride CRUD , confirm traveler with confirmation code)

--- server/controllers/RideController.js
+++ server/controllers/RideController.js
@@ -3,7 +3,25 @@ var user = require('../models/User');
 var traveler = require('../models/Traveler');
 var dis = require('google-distance');
 dis.apiKey = 'AIzaSyDtJlOlL_sZhchii9wg4A6yi7vZutilBeg';
+var SendEmail = require('./SendEmailController');
 
+
+function makeid(length) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function calculeTotal(pl, np) {
+
+    //mazalet 5edmet package
+    return pl * np
+
+}
 
 var getAll = (req, res, next) => {
     ride.find({}).sort('rideStartTime')
@@ -16,6 +34,7 @@ var getAll = (req, res, next) => {
 };
 
 var getById = (req, res, next) => {
+
     ride.findOne({"_id": req.params.id})
         .then((data) => {
             res.set('Content-Type', 'application/json');
@@ -38,37 +57,40 @@ var add = (req, res, next) => {
         function (err, data) {
             if (err) return console.log(err);
             var x = data['distanceValue'] / 1000;
-            var y = data['duration'];
-            console.log(data);
-            console.log(new Date(req.body.startTime));
-            console.log(new Date('02/9/2016').getTime()+data['durationValue']);
-            console.log();
-            console.log((new Date(new Date('02/9/2016').getTime()+data['durationValue'])));
-            console.log((new Date(1335205561117)));
 
-            ride.create({
-                status: req.body.status,
-                startTime: req.body.startTime,
-                origin: req.body.origin,
-                destination: req.body.destination,
-                nbrPlaces: req.body.nbrPlaces,
-                prixPerPlace: x * 0.06,
-                decription: req.body.decription,
-                total: 0,
-                packageAllowed: req.body.packageAllowed,
-                distance: x,
-                estimatedArriveTime :(new Date((new Date(req.body.startTime).getTime())+data['durationValue'])),
-                duration : data['duration']
-            })
-                .then((data) => {
-                    res.set('Content-Type', 'application/json');
-                    res.status(202).json(data);
+            user.findOne({"_id": req.params.idDriver})
+                .then((dr) => {
 
+                    ride.create({
+                        status: req.body.status,
+                        startTime: req.body.startTime,
+                        origin: req.body.origin,
+                        destination: req.body.destination,
+                        nbrPlaces: req.body.nbrPlaces,
+                        prixPerPlace: x * 0.06,
+                        decription: req.body.decription,
+                        total: 0,
+                        packageAllowed: req.body.packageAllowed,
+                        distance: x,
+                        estimatedArriveTime: (new Date((new Date(req.body.startTime).getTime()) + data['durationValue'])),
+                        duration: data['duration'],
+                        driver: dr
+                    })
+                        .then((data) => {
+                            res.set('Content-Type', 'application/json');
+                            res.status(202).json(data);
+
+                        })
+                        .catch(error => {
+                            res.set('Content-Type', 'text/html');
+                            res.status(500).send(error);
+                        });
+                    console.log(dr);
                 })
                 .catch(error => {
-                    res.set('Content-Type', 'text/html');
-                    res.status(500).send(error);
+                    console.log(error);
                 });
+
 
         });
 
@@ -129,38 +151,34 @@ var editRide = async (req, res, next) => {
                     rideToUpdate.startTime = req.body.startTime;
 
 
-
                 if (req.body.origin == null)
                     rideToUpdate.origin = rideToUpdate["origin"];
                 else
                     rideToUpdate.origin = req.body.origin;
 
 
-
                 if (req.body.destination == null)
                     rideToUpdate.destination = rideToUpdate["destination"];
                 else
-                rideToUpdate.destination = req.body.destination;
-
+                    rideToUpdate.destination = req.body.destination;
 
 
                 if (req.body.nbrPlaces == null)
                     rideToUpdate.nbrPlaces = rideToUpdate["nbrPlaces"];
                 else
-                rideToUpdate.nbrPlaces = req.body.nbrPlaces;
+                    rideToUpdate.nbrPlaces = req.body.nbrPlaces;
 
 
                 if (req.body.description == null)
                     rideToUpdate.description = rideToUpdate["description"];
                 else
-                rideToUpdate.description = req.body.description;
-
+                    rideToUpdate.description = req.body.description;
 
 
                 if (req.body.packageAllowed == null)
                     rideToUpdate.packageAllowed = rideToUpdate["packageAllowed"];
                 else
-                rideToUpdate.packageAllowed = req.body.packageAllowed;
+                    rideToUpdate.packageAllowed = req.body.packageAllowed;
 
 
                 rideToUpdate.duration = data['duration'];
@@ -192,43 +210,173 @@ var editRide = async (req, res, next) => {
 
 };
 
-function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-
 var addTravelerRide = async (req, res, next) => {
 
-    rideid = await ride.findById(req.params.idRide);
+    rideId = await ride.findById(req.params.idRide);
     userid = await user.findById(req.params.idUser);
+    var i;
+    exist = false;
+    for (i = 0; i < rideId.travelers.length; i++) {
+        if (userid.id.toString() === rideId.travelers[i].user._id.toString()) {
+            exist = true;
+        }
+    }
+    if (rideId['nbrPlaces'] > rideId.travelers.length) {
+        if (rideId.driver.toString() === userid.id.toString()) {
+            res.set('Content-Type', 'application/json');
+            res.status(301).send('it s your own carpool dude');
+        } else {
+            if (exist) {
+                res.set('Content-Type', 'application/json');
+                res.status(301).send('user alredy in this ride');
+            } else {
+                traveler.create({
+                    user: userid,
+                    confirmationCode: makeid(7),
+                }).then((data) => {
+                        ride.updateOne({'_id': req.params.idRide}, {"$addToSet": {"travelers": data}})
+                            .then((d) => {
+                                // sendemail lel d.driver , w data.user !!!
+                                res.set('Content-Type', 'application/json');
+                                res.status(202).json(d);
+                            })
+                            .catch(error => {
+                                res.set('Content-Type', 'application/json');
+                                res.status(500).send(error);
+                            });
+                    },
+                    (err) => console.log(err));
+            }
+        }
+    } else {
+        res.set('Content-Type', 'application/json');
+        res.status(301).send('full');
+    }
+};
 
-    trav = traveler.create({
-        user: req.body.status,
-        confimationCode: makeid(5),
-        valide: false,
+var getAllTravelers = (req, res, next) => {
 
-    });
+    ride.find({"_id": req.params.idRide}, {"travelers": 1, "_id": 0})
+        .then((data) => {
+            res.set('Content-Type', 'application/json');
+            res.status(202).json(data[0]);
+        })
+        .catch(error => {
+            res.set('Content-Type', 'application/json');
+            res.status(500).send(error);
+        });
+};
 
-    rideid.travelers = trav;
+var removeTravelerRide = async (req, res, next) => {
 
+    rideId = await ride.findById(req.params.idRide);
+    userid = await user.findById(req.params.idUser);
+    var i;
+    exist = false;
+    for (i = 0; i < rideId.travelers.length; i++) {
 
-    await rideid.save()
+        if (userid.id.toString() === rideId.travelers[i].user_id.toString()) {
+            exist = true;
+
+            ride.update({'_id': req.params.idRide}, {"$pull": {"travelers": rideId.travelers[i]}})
+                .then((d) => {
+                    // sendemail lel d.driver , w data.user !!!
+                    res.set('Content-Type', 'application/json');
+                    res.status(202).json(d);
+                })
+                .catch(error => {
+                    res.set('Content-Type', 'application/json');
+                    res.status(500).send(error);
+                });
+
+            traveler.deleteOne({"_id": rideId.travelers[i]._id})
+                .then(() => {
+                    res.set('Content-Type', 'application/json');
+                    res.status(202).send("traveler deleted");
+                })
+                .catch(error => {
+                    res.set('Content-Type', 'application/json');
+                    res.status(500).send(error);
+                });
+        }
+    }
+    if (!exist) {
+
+        res.set('Content-Type', 'application/json');
+        res.status(404).send("traveler not found");
+
+    }
+};
+
+var getRidesByDriver = (req, res, next) => {
+
+    ride.find({"driver": req.params.idUser})
         .then((data) => {
             res.set('Content-Type', 'application/json');
             res.status(202).json(data);
+        })
+        .catch(error => {
+            res.set('Content-Type', 'application/json');
+            res.status(500).send(error);
+        });
+};
 
+var confrimTraveler = async (req, res, next) => {
+
+    let rideToUpdate;
+    rideToUpdate = await ride.findById(req.params.idRide);
+    var i;
+    exist = false;
+    for (i = 0; i < rideToUpdate.travelers.length; i++) {
+        if (rideToUpdate.travelers[i].confirmationCode === req.params.validationCode) {
+            exist = true;
+            co = i;
+            traveler.updateOne({'_id': rideToUpdate.travelers[i]._id}, {"$set": {"valide": true}})
+                .then(() => {
+                    traveler.findById(rideToUpdate.travelers[co]._id).then((da) => {
+
+                        if (rideToUpdate.travelers[co].valide) {
+                            res.set('Content-Type', 'application/json');
+                            res.status(301).json('already valid');
+                        } else {
+                            rideToUpdate.travelers.splice(co, 1);
+                            rideToUpdate.travelers.push(da);
+                            rideToUpdate.placeConfirmed = rideToUpdate['placeConfirmed'] + 1;
+                            rideToUpdate.total = calculeTotal(rideToUpdate.prixPerPlace, rideToUpdate.placeConfirmed);
+                            rideToUpdate.save();
+                            res.set('Content-Type', 'application/json');
+                            res.status(202).json(rideToUpdate);
+                        }
+                    })
+                })
+                .catch(error => {
+                    res.set('Content-Type', 'application/json');
+                    res.status(500).send(error);
+                });
+        }
+    }
+    if (!exist) {
+        res.set('Content-Type', 'application/json');
+        res.status(301).send('no traveler exist with this code')
+    }
+
+};
+
+var getTraveler = (req, res, next) => {
+
+    traveler.findOne({"_id": req.params.idTraveler})
+        .then((data) => {
+            res.set('Content-Type', 'application/json');
+            res.status(202).json(data);
         })
         .catch(error => {
             res.set('Content-Type', 'text/html');
             res.status(500).send(error);
         });
 
+
 };
+
 
 module.exports = {
     getAll,
@@ -236,5 +384,10 @@ module.exports = {
     add,
     deleteRide,
     editRide,
-    addTravelerRide
+    addTravelerRide,
+    getAllTravelers,
+    removeTravelerRide,
+    getRidesByDriver,
+    confrimTraveler,
+    getTraveler
 };
