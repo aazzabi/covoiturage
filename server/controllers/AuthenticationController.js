296bdbcf2823b1e37de69ce4b17c22d23a37f7e0
Arafet AZZABI <arafet.azzabi@gmail.com>
2020-03-28 18:38:07 +0100

    List All users React

--- server/controllers/AuthenticationController.js
+++ server/controllers/AuthenticationController.js
@@ -112,7 +112,7 @@ var login = (req, res, next) => {
                     // return the information including token as JSON
                     res.json({
                         success: true,
-                        token: 'JWT ' + token
+                        token: token
                     });
                 } else {
                     res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
